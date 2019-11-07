07b2740970f73a324184ffc59a0473048e6992ff
ifac0 <ivanfelipealves@gmail.com>
2019-11-07 00:41:38 -0300

    adicionado api que apresenta valores sobre o clima e api que apresenta valores sobre poluição

--- front-end/spa-chartjs/src/app/app.component.ts
+++ front-end/spa-chartjs/src/app/app.component.ts
@@ -25,10 +25,56 @@ export class AppComponent {
   https://chrome.google.com/webstore/search/CORS
   */
   ngOnInit() {
-    //this.data = 
-    this.weather.getAll()
+   this.grafHist();
+  }
+
+  grafHist(){
+    this.weather.getHistorico()
     .subscribe(res => {
       console.log(res)
+
+      let temp_max = res['list'].map(res => res.main.temp_max);
+      let temp_min = res['list'].map(res => res.main.temp_min);
+      let alldates = res['list'].map(res => res.dt)
+
+      let weatherDates = []
+      alldates.forEach((res) => {
+          let jsdate = new Date(res * 1000)
+          weatherDates.push(jsdate.toLocaleTimeString('pt', { year: 'numeric', month: 'short', day: 'numeric' }))
+      })
+
+      this.chart = new Chart('hist', {
+          type: 'line',
+          data: {
+            labels: weatherDates,
+            datasets: [
+              { 
+                data: temp_max,
+                borderColor: "#3cba9f",
+                fill: false
+              },
+              { 
+                data: temp_min,
+                borderColor: "#ffcc00",
+                fill: false
+              },
+            ]
+          },
+          options: {
+            legend: {
+              display: false
+            },
+            scales: {
+              xAxes: [{
+                display: true
+              }],
+              yAxes: [{
+                display: true
+              }],
+            }
+          }
+        });
+
     }, err=> {
       console.log(err);
     });
--- front-end/spa-chartjs/src/app/weather.service.ts
+++ front-end/spa-chartjs/src/app/weather.service.ts
@@ -10,14 +10,26 @@ import { Observable } from 'rxjs';
 
 export class WeatherService {
 
-  private baseUrl = 'http://samples.openweathermap.org/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22';
+  /*
+  Fornecemos dados climáticos históricos das cidades
+  */
+  private baseHistorico =  'https://samples.openweathermap.org/data/2.5/history/city?lat=-28.4833&lon=-48.9941&appid=b1b15e88fa797225412429c1c50c122a1';
+  /*
+  Fornecemos dados sobre a poluição
+  */
+  private basePoluicao = 'https://samples.openweathermap.org/pollution/v1/co/0.0,10.0/2016-12-25T01:04:08Z.json?appid=5005b743d587e1573465d6a70f2b8ff4';
   
   constructor(private http: HttpClient){
   }
 
 
-  getAll(): Observable<any> {
-    return this.http.get(`${this.baseUrl}`);
+  getHistorico(): Observable<any> {
+    return this.http.get(`${this.baseHistorico}`);
+  }
+
+
+  getPoluicao(): Observable<any> {
+    return this.http.get(`${this.basePoluicao}`);
   }
 
 }
